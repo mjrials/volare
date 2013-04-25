4c038c566299c4f12080b0c5facb5b1b6608757d
Matthew Rials <matthew.rials@gmail.com>
2013-04-25 14:03:28 -0700

    functional results

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -6,29 +6,64 @@ function getNumTickets(string) {
 
 displayResults = function(data){
     console.log(data);
+    var userAirline = "United Airlines";
 
     $("#search_flights").fadeOut("normal", function() { 
         var html = [];
         html.push('<h2 class="sub_title">Search Results</h2>')
-        html.push('<table width="100%" border="0" cellspacing="0" cellpadding="3" class="table table-striped table-hover"><thead><tr><th>Flight Info</th><th>Route</th><th>Flight Times</th><th>Price</th><th>Cost</th><th>R. Miles</th><th>Q. Miles</th></tr></thead><tbody>');
+        html.push('<table width="100%" border="0" cellspacing="0" cellpadding="3" class="table table-striped table-hover"><thead><tr><th>Flight Info</th><th>Route</th><th>Flight Times</th><th>Price</th><th>R. Miles</th><th>Q. Miles</th><th>Cost</th></tr></thead><tbody>');
 
         var flights = data.response.itineraries;
         for (var i = 0; i < 10; i++) {
-            var code     = flights[i].outboundInfo.flightNumbers.join(' / ');
-            var route    = flights[i].outboundInfo.airports.join('-');
-            var times    = flights[i].outboundInfo.localArrivalTimeStr.substring(11, 16) + '-' +
-                            flights[i].outboundInfo.localDepartureTimeStr.substring(11, 16);
-            var price    = flights[i].price.totalAmount;
+            var airline         = (flights[i].carrier)[0].name;
+            var code            = flights[i].outboundInfo.flightNumbers.join(' / ');
+            var tClass          = (flights[i].outboundInfo.flightClasses)[0].name;
+            var route           = flights[i].outboundInfo.airports.join('-');
+            var times           = flights[i].outboundInfo.localArrivalTimeStr.substring(11, 16) + '-' +
+                                    flights[i].outboundInfo.localDepartureTimeStr.substring(11, 16);
+            var elapsedTimeMins = flights[i].outboundInfo.durationInMin;
+            var estMiles        = elapsedTimeMins * 8.546;          //EST 8.546 miles/min
+            var price           = flights[i].price.totalAmount;
+            var rMiles          = 0;
+            var qMiles          = 0;
+            var cost            = price;
+
+            if(airline == userAirline) {
+                if(tClass == 'Economy') {
+                    rMiles = estMiles;
+                }
+                if(tClass == 'Business') {
+                    rMiles = 1.25 * estMiles;
+                }
+                else { //First
+                    rMiles = 1.5 * estMiles;
+                }
+
+                if(rMiles < 500) {
+                    rMiles = 500;
+                }
+                if(rMiles > 2000) {
+                    cost = Math.round((price - (rMiles * 0.007)) * 100)/100;
+                }
+                else {
+                    cost = Math.round((price - (rMiles * 0.014)) * 100)/100;
+                }
+
+                $(".result_cost").css("color", "green"); 
+            }
                 
-            html.push('<tr><td>' + code + 
+            html.push('<tr><td>' + '<div><p class="result_airline">' + airline + '</p><p>' + code + ' - ' + tClass + '</p></div>' + 
                     '</td><td>' + route + 
                     '</td><td>' + times +
-                    '</td><td>' + price +
+                    '</td><td>$' + price +
+                    '</td><td>' + rMiles +
+                    '</td><td>' + qMiles +
+                    '</td><td>' + '<div><p class="result_cost">$' + cost + '</p></div>' +
                     '</td></tr>');
         }
 
-        html.push('</tbody></table>');
-
+        html.push('</tbody></table>');               
+        $("#search_flights").css("height", "605px"); 
         $("#search_flights").html(html.join(""));
     });    
     $("#search_flights").fadeIn("normal");
