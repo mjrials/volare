8cf6ad85b24c32e8bf287b90c55d873d37720a2a
Matthew Rials <matthew.rials@gmail.com>
2013-04-26 17:46:27 -0700

    dynamic content for user

--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -2,30 +2,40 @@ function oneWay() {
 	$("#oneway").addClass("active");
 	$("#roundtrip").removeClass("active");
 	$(".arrive_date").fadeOut("normal");
+
+	$("#triptype").val("oneWay");
 }
 
 function roundTrip() {
 	$("#roundtrip").addClass("active");
 	$("#oneway").removeClass("active");
 	$(".arrive_date").fadeIn("normal");
+
+	$("#triptype").val("roundTrip");
 }
 
 function coach() {
 	$("#coach").addClass("active");
 	$("#firstclass").removeClass("active");
 	$("#business").removeClass("active");
+
+	$("#class_select").val("Economy");
 }
 
 function business() {
 	$("#business").addClass("active");
 	$("#firstclass").removeClass("active");
 	$("#coach").removeClass("active");
+
+	$("#class_select").val("Business");
 }
 
 function firstclass() {
 	$("#firstclass").addClass("active");
 	$("#coach").removeClass("active");
 	$("#business").removeClass("active");
+
+	$("#class_select").val("First");
 }
 
 // function airportAC() {
--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,19 +1,37 @@
 //  48a5cb3a2dc46b8aab6f                    --  wego api key
+//Fix   pendingResults
+//      get rid of layover time
+//      referrals to regular site
+//      register + login + Ff thing
+//      q miles
+//      green cost text
+//      switch to dnsimple -> link to heroku
+//      integrate designsuxx table css
+//      anonymous search (FF input box)
+//      verify well-formed input
+//      and highlight boxes in red
+//      roundTrip not just oneWay -- multipel flight times, layover, etc
+//      add pics for airlines?
+
 
 function getNumTickets(string) {
     return string.replace( /^\D+/g, '');
 }
 
 function tConvert (time) {
-  // Check correct time format and split into components
   time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
-
-  if (time.length > 1) { // If time format correct
-    time = time.slice (1);  // Remove full string match value
-    time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
-    time[0] = +time[0] % 12 || 12; // Adjust hours
+  if (time.length > 1) { 
+    time = time.slice (1);  
+    time[5] = +time[0] < 12 ? 'AM' : 'PM'; 
+    time[0] = +time[0] % 12 || 12; 
   }
-  return time.join (''); // return adjusted time or original string
+  return time.join ('');
+}
+
+function camelCase(input) { 
+    return input.toLowerCase().replace(/ (.)/g, function(match, group1) {
+        return group1.toUpperCase();
+    });
 }
 
 displayResults = function(data){
@@ -22,8 +40,8 @@ displayResults = function(data){
 
     $("#search_flights").fadeOut("normal", function() { 
         var html = [];
-        html.push('<h2 class="sub_title">Search Results</h2>')
-        html.push('<table width="100%" border="0" cellspacing="0" cellpadding="3" class="table table-striped table-hover"><thead><tr><th>Flight Info</th><th>Route</th><th>Flight Times</th><th>Price</th><th>R. Miles</th><th>Q. Miles</th><th>Cost</th></tr></thead><tbody>');
+        html.push('<h2 class="sub_title">Top Results</h2>')
+        html.push('<table width="100%" border="0" cellspacing="0" cellpadding="3" class="table table-striped table-hover"><thead><tr><th>Flight Info</th><th>Route</th><th class="center">Flight Times</th><th>Price</th><th>R. Miles</th><th>Q. Miles</th><th>Net Cost</th></tr></thead><tbody>');
 
         var flights = data.response.itineraries;
         for (var i = 0; i < 10; i++) {
@@ -31,10 +49,10 @@ displayResults = function(data){
             var code            = flights[i].outboundInfo.flightNumbers.join(' / ');
             var tClass          = (flights[i].outboundInfo.flightClasses)[0].name;
             var route           = flights[i].outboundInfo.airports.join('-');
-            var times           = tConvert(flights[i].outboundInfo.localArrivalTimeStr.substring(11, 16)) + '-' +
-                                    tConvert(flights[i].outboundInfo.localDepartureTimeStr.substring(11, 16));
+            var times           = tConvert(flights[i].outboundInfo.localDepartureTimeStr.substring(11, 16)) + ' - ' +
+                                    tConvert(flights[i].outboundInfo.localArrivalTimeStr.substring(11, 16));
             var elapsedTimeMins = flights[i].outboundInfo.durationInMin;
-            var estMiles        = elapsedTimeMins * 8.546;          //EST 8.546 miles/min
+            var estMiles        = elapsedTimeMins * 8.546;          
             var price           = Math.round(flights[i].price.totalAmount);
             var rMiles          = 0;
             var qMiles          = 0;
@@ -45,8 +63,8 @@ displayResults = function(data){
                 var dollarsPerMile;
 
                 if(tClass == 'Economy')     { rMiles = estMiles; }
-                if(tClass == 'Business')    { rMiles = 1.25 * estMiles; }
-                else                        { rMiles = 1.5 * estMiles; }
+                if(tClass == 'Business')    { rMiles = Math.round(1.25 * estMiles); }
+                else                        { rMiles = Math.round(1.5 * estMiles); }
 
                 if(rMiles < 500)            { rMiles = 500; }
                 if(rMiles > 2000)           { dollarsPerMile = 0.007; }
@@ -59,7 +77,7 @@ displayResults = function(data){
                 
             html.push('<tr><td>' + '<div><p class="result_airline">' + airline + '</p><p>' + code + ' - ' + tClass + '</p></div>' + 
                     '</td><td>' + route + 
-                    '</td><td>' + times +
+                    '</td><td>' + '<div><p class="center">' + times + '</p></div>' + 
                     '</td><td>$' + price +
                     '</td><td>' + '<div><p>' + rMiles + '</p><p> - $' + rewardDeduction + '</p></div>' +
                     '</td><td>' + qMiles +
@@ -90,29 +108,35 @@ pull = function(data){
 function startSearch(event)
 {
     //scrape parameters
-    var tClass = $('.class_select').val();          //must be Economy, Business, First
-    // var tClass = $('#class_select li.selected a').text();
-    var type = $('.triptype').val();                //must be oneWay || roundTrip
-    var from = $('.depart_from').val();             //make sure these are IATA codes
-    var to = $('.arrive_to').val();                 
-    var depart = $('.depart_date').val();
-    var arrive = $('.arrive_date').val();           //dates must be yyyy-mm-dd
-    var adults = $('.adults_select').val();         //must be number
-    var children = $('.children_select').val();
+    // var tClass = $('.class_select').val();          //must be Economy, Business, First
+    var tClass      = $('.class_select li.active a').text();
+    var type        = camelCase($('.triptype li.active a').text());                //must be oneWay || roundTrip
+    var from        = $('.departfrom').val();             //make sure these are IATA codes
+    var to          = $('.arriveto').val();                 
+    var depart      = $('.departdate').val();
+    var arrive      = $('.arrivedate').val();           //dates must be yyyy-mm-dd
+    var adults      = $('.adults_select').val();         //must be number
+    var children    = $('.children_select').val();
+
+    var arriveString = '';
+    if(type == "roundTrip") {
+        arriveString = '&inboundDate=' + arrive;
+    }
 
     // verify well-formed input
 
     // startSearch with instanceId
     var instanceURL = 'http://www.wego.com/api/flights/startSearch.html?' +
                     'format=json' +
-                    '&fromLocation=SFO' +
-                    '&toLocation=ORD' +
-                    '&tripType=oneWay' +
-                    '&cabinClass=Economy' +
-                    '&outboundDate=2013-06-09' +
-                    '&numAdults='+ adults +
-                    '&numChildren='+ children + 
-                    '&callback=pull' + 
+                    '&fromLocation='    + from + 
+                    '&toLocation='      + to + 
+                    '&tripType='        + type + 
+                    '&cabinClass='      + tClass + 
+                    '&outboundDate='    + depart + 
+                    arriveString        + 
+                    '&numAdults='       + adults +
+                    '&numChildren='     + children + 
+                    '&callback=pull'    + 
                     '&apiKey=48a5cb3a2dc46b8aab6f'
     $.ajax({
         url: instanceURL,
