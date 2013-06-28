b085ba466f7e276f79cd3a5ae81f9737126600f9
Matthew Rials <matthew.rials@gmail.com>
2013-06-28 12:43:00 -0400

    more attempted changes for pg

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Volare
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
+    #ADDED MJR 6/28
   end
 end
